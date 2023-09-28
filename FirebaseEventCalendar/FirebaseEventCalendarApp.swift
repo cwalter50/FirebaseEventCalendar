e33dc567144c7f95404cefce957a780ef4403aef
cwalter50 <walt50@gmail.com>
2023-09-28 13:27:20 -0400

    Initial Commit

---
+++ FirebaseEventCalendar/FirebaseEventCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseEventCalendarApp.swift
+//  FirebaseEventCalendar
+//
+//  Created by Christopher Walter on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseEventCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
